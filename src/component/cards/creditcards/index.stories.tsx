3bbbb8c75c05b8d5357d683e0abfc8cd93f2432f
Christopher Igbojekwe <christopher.igbojekwe@gmail.com>
2022-12-15 15:16:04 -0600

    add credit card

---
+++ src/component/cards/creditcards/index.stories.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import CreditCard from "./";
+import { ThemeColor } from "./";
+import styled from "styled-components";
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: "New/CreditCard",
+  component: CreditCard,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    themeColor: {
+      options: ["default", "green", "brown"],
+      control: { type: "select" }, // Automatically inferred when 'options' is defined
+    },
+  },
+} as ComponentMeta<typeof CreditCard>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof CreditCard> = (args) => (
+  <CreditCard {...args} />
+);
+
+const Group: ComponentStory<typeof CreditCard> = (args) => (
+  <CreditCard {...args} />
+);
+
+export const Default = Template.bind({});
+
+export const Stacked = () => {
+  return (
+    <StackedWrapper>
+      <Template />
+      <GreenCard>
+        <Template themeColor="green" />
+      </GreenCard>
+      <BrownCard>
+        <Template themeColor="brown" />
+      </BrownCard>
+    </StackedWrapper>
+  );
+};
+
+const StackedWrapper = styled.div`
+  position: relative;
+`;
+
+const GreenCard = styled.div`
+  position: fixed;
+  top: 100px;
+`;
+
+const BrownCard = styled.div`
+  position: fixed;
+  top: 200px;
+`;
