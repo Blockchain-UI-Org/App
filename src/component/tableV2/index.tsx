c181235f57af3005f40d38a5b35eb5ae420b5cbf
Christopher Igbojekwe <christopher.igbojekwe@gmail.com>
2022-12-18 01:13:34 -0600

    start building table component

---
+++ src/component/tableV2/index.tsx
@@ -0,0 +1,11 @@
+import Table from "@mui/material/Table";
+import TableContainer from "@mui/material/TableContainer";
+import TableRow from "@mui/material/TableRow";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+
+const TableComponent = () => {
+  return <Table></Table>;
+};
+
+export default TableComponent;
