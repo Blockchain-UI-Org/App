28307161228c0dfd55507aebe7cceef20a0bd8a2
Adarshdeep <adarshdeep@iwebcode.design>
2023-02-10 23:44:52 -0700

    Fixed Mono Repo Based Bui

--- packages/blockchain-ui/.storybook/main.js
+++ packages/blockchain-ui/.storybook/main.js
@@ -1,3 +1,5 @@
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
@@ -14,5 +16,12 @@ module.exports = {
   framework: "@storybook/react",
   core: {
     builder: "@storybook/builder-webpack5",
+  },
+  webpackFinal: async (config, { configType }) => {
+    if(!config.resolve.plugins){
+      config.resolve.plugins = []
+    }
+    config.resolve.plugins.push(new TsconfigPathsPlugin())
+    return config;
   },
 };
