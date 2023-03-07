8df7f15852e6e6d2792489e925c7185b9410e260
Bilal khatri <khatribilal5@gmail.com>
2023-03-08 03:18:15 +0500

    Fixed useTheme issue to access window

--- packages/blockchain-ui/src/theme/useTheme.ts
+++ packages/blockchain-ui/src/theme/useTheme.ts
@@ -1,14 +1,20 @@
+import { TColorSchemes } from "blockchain-ui/typeutils/theme";
+import { useEffect, useState } from "react";
 import { useTheme as useStyledTheme } from "styled-components";
 import { appThemes, DefaultTheme, ThemeInterface } from "./theme";
 
 export const useTheme = (): ThemeInterface => {
   const theme = useStyledTheme();
-  /**
-   * if theme not exists ..
-   * look for color scheme globally defined in root..
-   */
-  const defaultThemeName =
-    window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+  const [themeName, setThemeName] = useState<TColorSchemes | null>();
+  useEffect(() => {
+    /**
+     * if theme not exists ..
+     * look for color scheme globally defined in root..
+     */
+    const defaultThemeName =
+      window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+    setThemeName(defaultThemeName);
+  }, []);
 
-  return (theme as ThemeInterface) || (defaultThemeName ? appThemes[defaultThemeName] : DefaultTheme);
+  return (theme as ThemeInterface) || (themeName ? appThemes[themeName] : DefaultTheme);
 };
